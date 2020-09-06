d2c4c1019b6e6583f43f1e152c4a74af2bf17108
Abdurrahman <abduqah@gmail.com>
2020-09-06 17:34:22 +0200

    Added new service uses GitHub new API v4 Added results from new service to GraphQL-statistics.json file

---
+++ main-graphql.py
@@ -0,0 +1,73 @@
+import requests
+import json
+from sys import exit
+from datetime import date, timedelta
+from decouple import config
+from string import Template
+
+def main():
+  check_token_exists()
+
+  date_30days_ago = date.today() - timedelta(30)
+
+  response = call_github_api(date_30days_ago)
+  return get_statistics(response)
+
+def call_github_api(date):
+  print('Real process started', sep='\n')
+
+  query = Template("""{
+    search(type: REPOSITORY, query: "created:>$data sort:stars-desc", first: $numOfResults) {
+      nodes {
+        ... on Repository {
+          name
+          url
+          languages(first: 100) {
+            nodes {
+              name
+            }
+          }
+        }
+      }
+    }
+  }""")
+
+  date_30days_ago = date.today() - timedelta(30)
+
+  r = requests.post("https://api.github.com/graphql", json={'query': query.substitute(data=date_30days_ago, numOfResults=100)},headers={'Authorization': 'token %s' % config('TOKEN')})
+  json_data = json.loads(r.text)
+
+  return json_data['data']['search']['nodes']
+
+
+def get_statistics(data):
+  languages = {}
+
+  for repo in data:
+    for lang in repo['languages']['nodes']:
+      try:
+        language_obj = languages[lang['name']]
+        language_obj['count'] += 1
+        language_obj['repos_list'].append(repo['url'])
+      except:
+        languages[lang['name']] = {'count': 1, 'repos_list': [repo['url']]}
+        print('New language added: %s' % lang['name'], sep='\n')
+        pass
+
+  return languages
+
+def check_token_exists():
+  try:
+    if len(config('TOKEN')) == 0:
+      print('Please add valid token')
+      exit(1)
+  except:
+    print('Please add .env file with token provided inside. check .env.example')
+    exit(1)
+    pass
+  
+
+
+with open('GraphQL-statistics.json', 'w') as file:
+  json.dump(main(), file)
+
